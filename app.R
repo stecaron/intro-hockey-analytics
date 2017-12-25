8b75c51dc9a368e3f368e6831726f39f9e1d1a47
Stephane Caron <ste.caron@icloud.com>
2017-12-25 13:34:07 -0500

    Reset main panel of the app

--- app.R
+++ app.R
@@ -8,6 +8,7 @@
 library(shiny)
 library(ggplot2)
 library(ggthemes)
+library(broom)
 
 # Load the data -----------------------------------------------------------
 
@@ -21,42 +22,63 @@ ui <- fluidPage(
    # Application title
    titlePanel("Hockey players kmeans clustering"),
    
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("clusters",
-                     "Number of clusters:",
-                     min = 1,
-                     max = 10,
-                     value = 5),
-         selectizeInput(inputId = "variables", 
-                            label = "Variables to include in the clustering:", 
-                            choices = colnames(data_final)[c(8:ncol(data_final))], 
-                            multiple = TRUE,
-                            selected = c("G", "A", "PTS", "TOI_GP", "Hits")),
-         selectizeInput(inputId = "team", 
-                        "Teams:", 
-                        choices = unique(data_final$Team), 
-                        selected = "MTL", 
-                        multiple = TRUE),
-         selectizeInput(inputId = "position", 
-                        "Positions:", 
-                        choices = unique(data_final$Position), 
-                        selected = c("C", "LW", "RW", "D"), 
-                        multiple = TRUE)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("clusterPlot")
-      )
+   fluidRow(
+     column(5,
+            sliderInput("clusters",
+                        "Number of clusters:",
+                        min = 1,
+                        max = 10,
+                        value = 5),
+            selectizeInput(inputId = "variables", 
+                           label = "Variables to include in the clustering:", 
+                           choices = colnames(data_final)[c(8:ncol(data_final))], 
+                           multiple = TRUE,
+                           selected = c("G", "A", "PTS", "TOI_GP", "Hits")),
+            selectizeInput(inputId = "team", 
+                           "Teams:", 
+                           choices = unique(data_final$Team), 
+                           selected = "MTL", 
+                           multiple = TRUE),
+            selectizeInput(inputId = "position", 
+                           "Positions:", 
+                           choices = unique(data_final$Position), 
+                           selected = c("C", "LW", "RW", "D"), 
+                           multiple = TRUE)
+     ),
+     column(7,
+       plotOutput("clusterElbow")
+     )
+   ),
+   fluidRow(
+     plotOutput("clusterPlot") 
+     ),
+   hr(),
+   fluidRow(
+     selectizeInput(inputId = "x_axis", 
+                 label = "x-asis variable:", 
+                 choices = c("G", "A", "PTS", "TOI_GP", "Hits"),
+                 multiple = FALSE),
+     selectizeInput(inputId = "y_axis", 
+                 label = "y-asis variable:",
+                 choices = c("G", "A", "PTS", "TOI_GP", "Hits"),
+                 multiple = FALSE)
    )
 )
 
 
 # Define server -----------------------------------------------------------
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  observe({
+    input$variables
+    updateSelectizeInput(session, "x_axis",
+                      choices = input$variables,
+                      selected = input$variables[1])
+    updateSelectizeInput(session, "y_axis",
+                      choices = input$variables,
+                      selected = input$variables[2])
+    })
   
   number_clusters <- reactive({
     input$clusters
@@ -77,11 +99,33 @@ server <- function(input, output) {
       )
   })
   
+  clusters_analysis <- reactive({
+    clusters_analysis <- data.frame(k = 1:9) %>% 
+                          group_by(k) %>% 
+                          do(kclust = kmeans(data_clustering(), .$k)) %>% 
+                          group_by(k) %>%
+                          do(glance(.$kclust[[1]]))
+  })
+  
+  output$clusterElbow <- renderPlot({
+    ggplot(clusters_analysis(), aes(k, tot.withinss)) + 
+      geom_line(size = 2) +
+      scale_x_continuous(name = "Number of clusters", breaks = c(1:9)) +
+      scale_y_continuous("Total within Sum of Squares") +
+      theme_classic()
+      
+  })
+  
+  axis <- reactive({
+    list(x = input$x_axis,
+         y = input$y_axis)
+  })
+  
   output$clusterPlot <- renderPlot({
-      ggplot(data_output(), aes(x = Shot.SlapShot, y = TOI_GP, label = Last_Name, color = as.factor(cluster))) +
-        geom_label() +
-        theme_classic() +
-        theme(legend.position = "none")
+    ggplot(data_output(), aes_string(x = axis()$x, y = axis()$y, label = "Last_Name", color = as.factor("cluster"))) +
+      geom_label() +
+      theme_classic() +
+      theme(legend.position = "none")
    })
 }
 
